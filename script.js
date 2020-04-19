8ff034c948aafa2618f787cc227bdb7059c5b00c
MasamichiKanazawa <61020745+MasamichiKanazawa@users.noreply.github.com>
2020-04-20 01:06:31 +0900

    aaa

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
 $(function () {
-  //現在のマス　サイコロの結果を加算していく
+  //現在のマス　サイコロの結果　加算していく
   var mathPosition = 0;
   var mathPositionId = "math" + mathPosition;
-  //ゴールまでのマス
-  var goalPosition = 20;
+  //ゴールまでのマス サイコロの結果　減少していく
+  var goalPosition = 100;
   //サイコロを振った結果
   var min = 1;
   var max = 6;
-  // var diceResult = Math.floor(Math.random() * (max + 1 - min)) + min;
   //サイコロの画像
-  var resultImage = "1.png";
-  //サイコロを振るのが何回目か
+  // var resultImage = "1.png";
+  // //サイコロを振るのが何回目か
   var diceRollCount = 1;
   // コマが移動する数
   var moveCount;
@@ -18,24 +17,47 @@ $(function () {
   //サイコロの結果を取得
   function diceAction() {
     diceResult = Math.floor(Math.random() * (max + 1 - min)) + min;
+    // alert(diceResult);
     $(`img`).attr(`src`, `${diceResult}.png`);
   }
 
-  // マスの位置を取得1,駒の移動
+  // マスの位置を取得,コマの移動
   function position() {
-    mathPosition = mathPosition + diceResult;
-    document.getElementById(math0).innerHTML = "";
-    document.getElementById(mathPositionId).innerHTML = "";
-    document.getElementById(
-      mathPositionId
-    ).innerHTML = `<i id="nowPosition" i class="fas fa-horse"></i>`;
+    if (mathPosition < 100) {
+      mathPosition = mathPosition + diceResult;
+      // alert(mathPosition);
+      // コマの移動
+      document.getElementById(mathPositionId).innerHTML = "";
+      mathPositionId = "math" + mathPosition;
+      document.getElementById(
+        mathPositionId
+      ).innerHTML = `<i id="nowPosition" i class="fas fa-horse"></i>`;
+      // 画面を自動スクロール
+      let element = document.getElementById("nowPosition");
+      element.scrollIntoView({ behavior: "smooth", inline: "start" });
+      // ゴールまでのマスを取得、表示
+      goalPosition = goalPosition - diceResult;
+      // alert(goalPosition);
+      $(`＃toGoal`).text(`ゴールまであと${goalPosition}マス`);
+      // サイコロを振った回数を追加、表示
+      diceRollCount++;
+      // alert(diceRollCount);
+      $(`＃count`).text(`${diceRollCount}投目`);
+    } else {
+      // // ゴール コマを変化させる、コメント表示
+      // document.getElementById(mathPositionId).innerHTML = "";
+      // mathPositionId = "math" + mathPosition;
+      // document.getElementById(
+      //   mathPositionId
+      // ).innerHTML = `<i id="nowPosition" i class="fas fa-chess-knigh"></i>`;
+      $(`＃toGoal`).text(`ゴール！！`);
+    }
   }
 
   // サイコロを振る
   $("#dicerollBtn").click(function () {
     diceAction(); //サイコロの結果を取得
-    position(); // マスの位置を取得
-    // function move();// 駒を動かす
+    position(); // マスの位置を取得、コマの移動
     // diceAnimation();//サイコロの動き
   });
 
