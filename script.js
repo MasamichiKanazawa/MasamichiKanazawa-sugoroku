b88c0085759cb8e4f2a2bb56705fb79724a101ef
MasamichiKanazawa <61020745+MasamichiKanazawa@users.noreply.github.com>
2020-04-20 14:33:48 +0900

    aaa

--- script.js
+++ script.js
@@ -11,6 +11,8 @@ $(function () {
   var diceRollCount = 1;
   // コマが移動する数（イベント用）
   var moveCount;
+  var minMove = 1;
+  var maxMove = 10;
 
   //サイコロの結果を取得
   function diceAction() {
@@ -26,19 +28,23 @@ $(function () {
       // マスの位置を取得
       mathPosition = mathPosition + diceResult;
       // alert(mathPosition);
+
       // コマの移動
       document.getElementById(mathPositionId).innerHTML = "";
       mathPositionId = "math" + mathPosition;
       document.getElementById(
         mathPositionId
       ).innerHTML = `<i id="nowPosition" i class="fas fa-horse"></i>`;
+
       // 画面を自動スクロール
       let element = document.getElementById("nowPosition");
       element.scrollIntoView({ behavior: "smooth", inline: "start" });
+
       // ゴールまでの残りマスを取得、表示
       goalPosition = goalPosition - diceResult;
       $(`＃toGoal`).text(`ゴールまであと${goalPosition}マス`);
       // alert(goalPosition);
+
       // サイコロを振った回数を追加、表示
       diceRollCount++;
       $(`＃count`).text(`${diceRollCount}投目`);
@@ -53,11 +59,38 @@ $(function () {
       $(`＃toGoal`).text(`ゴール！！`);
     }
   }
+  
+  function fowordEvent(){
+    moveCount = Math.floor(Math.random() * (maxMovex + 1 - mminMove)) + minMove;
+    alert(moveCount);
+  };
+
+  function backEvent(){
+    moveCount = Math.floor(Math.random() * (maxMove + 1 - minMove)) + minMoves;
+    alert(moveCount);
+  };
+
+  // イベントの発生、条件分岐
+  function selectEvent() {
+    if (
+      document.getElementById(mathPositionId).classList.contains("event") ==
+      true
+    ) {
+      selectId = Math.floor(Math.random() * 2 + 1);
+      alert(selectId);
+      if (selectId == 1) {
+        function fowordEvent();
+      } else {
+        function backEvent();
+      }
+    }
+  }
 
   // サイコロを振る　まとめ
   $("#dicerollBtn").click(function () {
     diceAction(); //サイコロの結果を取得
     position(); // マスの位置を取得、コマの移動
+    selectEvent(); // イベントの選択
     // diceAnimation();//サイコロの動き
   });
 
